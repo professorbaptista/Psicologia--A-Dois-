a55d7531c24397bb6f64aef06fc511b8918fa63f
José Baptista <148701836+professorbaptista@users.noreply.github.com>
2025-07-02 14:30:06 +0100

    Conclusão da pagina depoimentos e  serviços

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -18,7 +18,7 @@ router.get('/about', async (req, res) => {
 
 router.get('/services', async (req, res) => {
 
-    res.render('services', { titulo: 'Página Services'})
+    res.render('services', { titulo: 'Meus Serviços', services: 'José Baptista | Psicólogo'}) 
 });
 
 router.get('/blogue', async (req, res) => {
@@ -26,19 +26,43 @@ router.get('/blogue', async (req, res) => {
     res.render('blogue', {titulo: 'Recomeçar com Propósito'})
 });
 
-router.get('/depoi', (req, res) => {
-    
+// Rota depoimentos
+let depoimentos = [
+    {name: 'Ana A.', message: 'A terapia com Dr. Baptista transformou minha vida. Bem haja, o Dr. Baptista!', image: 'img'},
+    {name: 'Carlos M.', message: 'Aprendi a me comunicar melhor com a minha parceira! Obrigado Dr. Baptista', image: 'img'}
+];
 
-    res.render('depoi', {titulo: 'Depoimentos dos clientes satisfeitos e felizes', name: 'Jose', depoimentos: 'Muito satisfeito com o atendimento do Doutor. Ele salvou o meu casamento que estava a beira do colapso. Recomento...' })
+router.get('/depoi', async (req, res) => {
+    
+    res.render('depoi', { depoimentos, titulo: 'Depoimentos dos Clientes Satisfeitos e Felizes' });
+});
+
+router.post('/adicionar', async (req, res) => {
+
+    const { name, message } = req.body;
+
+    if(name && message){
+        depoimentos.push({ name, message })
+    }
+
+    console.log(req.body)
+    res.redirect('/depoi')
 })
+
+
 router.get('/contacto', (req, res) => {
 
-    res.render('contactos', {titulo: 'Entre em Contacto Agora'})
+    res.render('contactos', {titulo: 'Entre em Contacto Agora mesmo, não adie mais o seu problema!'})
 });
 
 router.get('/agendar', async (req, res) => {
 
     res.render('agendarConversa', {titulo: 'Agende uma Conversa'})
+});
+
+router.post('/agendarConsulta', async (req, res) => {
+
+    res.redirect('/agendar')
 })
 
 module.exports = router;
