e073b670559cf711a3ace62dd146011af6ee3294
José Baptista <148701836+professorbaptista@users.noreply.github.com>
2025-07-05 02:59:02 +0100

    Reajustes de pagina de depoimentos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const moment = require('moment');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -27,15 +29,20 @@ router.get('/blogue', async (req, res) => {
 });
 
 // Rota depoimentos
-let depoimentos = [
-    {name: 'Ana A.', message: 'A terapia com Dr. Baptista transformou minha vida. Bem haja, o Dr. Baptista!', image: 'img'},
-    {name: 'Carlos M.', message: 'Aprendi a me comunicar melhor com a minha parceira! Obrigado Dr. Baptista', image: 'img'}
-];
+router.get('/deixarDepoimento', (req, res) => {
+
+    res.render('deixarDepoimento', {titulo: 'Deixe o seu depoimento'})
+});
+
+let depoimentos = [];
+let comments = [];
 
 router.get('/depoi', async (req, res) => {
+    const id = req.params.id
     
-    res.render('depoi', { depoimentos, titulo: 'Depoimentos dos Clientes Satisfeitos e Felizes' });
+    res.render('depoi', { depoimentos, comments, titulo: 'Depoimentos dos Clientes Satisfeitos e Felizes', id });
 });
+
 
 router.post('/adicionar', async (req, res) => {
 
@@ -49,20 +56,86 @@ router.post('/adicionar', async (req, res) => {
     res.redirect('/depoi')
 })
 
-
+let contactos = [];
 router.get('/contacto', (req, res) => {
 
     res.render('contactos', {titulo: 'Entre em Contacto Agora mesmo, não adie mais o seu problema!'})
 });
 
+router.post('/contactos', async (req, res) => {
+    try {
+
+        const { name, email, message } = req.body;
+        contactos.push({ name, email, message})
+        console.log('contactos:', contactos)
+        res.redirect('/agendamento')
+    } catch (error) {
+        res.render('contactos', {error: 'Erro ao enviar os contactos'})
+    }
+
+    
+})
+
+
+
 router.get('/agendar', async (req, res) => {
 
-    res.render('agendarConversa', {titulo: 'Agende uma Conversa'})
+    res.render('agendarConversa', {titulo: 'Agende um serviço'})
 });
 
+let agendamentos = [];
+
+router.get('/agendamento', async (req, res) => {
+
+    res.render('agendamento', { agendamentos, contactos, message: 'Agendamento efectuado com sucesso!', titulo: 'Agendamentos', contato: 'Contactos', comments }) 
+});
+
+ 
 router.post('/agendarConsulta', async (req, res) => {
 
-    res.redirect('/agendar')
+  try {
+    
+    const { name, data, hora, tipo_servicos, message } = req.body;
+    agendamentos.push({ name, data, tipo_servicos, hora, message})
+    res.redirect('/agendamento');
+
+  } catch (error) {
+    res.render('agendar', { error: 'Erro ao agendar consulta...'})
+  }
+});
+
+router.post('/comentarios/:id', async (req, res) => {
+    
+    try {
+        
+        const { name, comment } = req.body;
+       
+        const id = req.params.id;
+        console.log('Id: ', id)
+
+        const depoimento = depoimentos.find(d => d.id === id);
+         
+        console.log('Depoimento: ', depoimento)
+        if (depoimento && name && comment) {
+            depoimentos.push({depoimento, name, comment});
+
+            const data = moment().format('DD/MM/YYYY HH:MM');
+
+            const novoComentario = {
+                name,
+                comment,
+                data, 
+                foto: '/users/usuario.png'
+            }
+
+            comments.push(novoComentario);
+            console.log(novoComentario)
+            res.redirect('/depoi')
+        }
+    } catch (error) {
+        res.render('erro', {error: 'Erro ao enviar o comentário!'}) 
+        
+    }
 })
 
 module.exports = router;
